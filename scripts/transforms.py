4fa3a6c957029fdc550a74e75bb87e986cba58d4
Mathieu Comandon <strycore@gmail.com>
2015-06-20 23:44:43 -0700

    Initial working version

---
+++ scripts/transforms.py
@@ -0,0 +1,12 @@
+import math
+
+
+selector = ".dial"
+l = 8
+for i in range(1, 13):
+    a = math.radians((i - 3) * 30)
+    x = math.cos(a) * l
+    y = math.sin(a) * l
+    print("{} :nth-child({}) {{ transform: translate({:.2f}em, {:.2f}em); }}".format(
+        selector, i, x, y
+    ))
